dbf86091399b627a7386913ebbd4dec74dcc562c
whitepink4514-cell <whitepink4514@gmail.com>
2026-04-25 12:00:29 +0800

    Day1 python基本语法1

--- test.py
+++ test.py
@@ -1,1 +1,46 @@
+"""
+多行注释
+550/5的结果
+直接打印550/5
+"""
+#注释
+"""
 print("Hello World")
+print(550/5)
+print("550/5")
+print("直接打印结果为：",550/5)
+"""
+#定义一个变量，用来记录钱包的余额
+money = 50
+#print("钱包还有：" , money)
+#买了一个冰淇淋，花了10元
+#money = money - 10
+#print("我的钱包里还剩",money,"元")
+"""
+a = 50
+b = 100
+print(a*b)
+print(a/b)
+print(a+b)
+print(a-b)
+"""
+"""
+法一：查看数据类型
+print(type("你好，世界"))
+print(type(111))
+print(type(12.23))
+"""
+
+"""
+法二：查看数据类型
+string_type = type("你好，世界")
+int_type = type(111)
+float_type = type(12.12)
+print(string_type)
+print(int_type)
+print(float_type)
+"""
+
+name = "你好，世界"
+string_type = type(name)
+print(string_type)
